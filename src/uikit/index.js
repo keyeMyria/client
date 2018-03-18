050c3de761ce5daa30bb1d89230d332c9cfe5c6d
Sygeman <sygeman@gmail.com>
2018-03-19 00:07:35 +0300

    role set fix

--- src/uikit/index.js
+++ src/uikit/index.js
@@ -4,5 +4,6 @@ export * from './switch';
 export * from './modal';
 // export FullModal from './modalFull';
 export * from './tabs';
+export * from './dropdown';
 export * from './dropdownMenu';
 export * from './caret';
