66f7c9ebd67ed55991b5703bc58ce38791f00c28
Sygeman <sygeman@gmail.com>
2018-03-20 03:51:39 +0300

    query getFollowRooms

---
+++ src/queries/getFollowRooms.js
@@ -0,0 +1,27 @@
+import { apolloClient } from 'utils/apolloClient';
+import gql from 'graphql-tag';
+import { followRoomsStore } from 'stores';
+
+export const getFollowRooms = async () => {
+  const { data, errors } = await apolloClient.query({
+    query: gql`
+      query getFollowRooms {
+        getFollowRooms {
+          id
+          name
+          title
+          avatar
+          usersCount
+          guestsCount
+          contentTitle
+        }
+      }
+    `
+  });
+
+  if (errors) {
+    return console.error(errors);
+  }
+
+  followRoomsStore.rooms = data.getFollowRooms;
+}
